d19fb5e3d7cb5029543156b4bcecb2f79541ec01
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:16:37 +0000

    Implemented destroy action for portfolio feature

--- DevCampPortf/app/controllers/portfolios_controller.rb
+++ DevCampPortf/app/controllers/portfolios_controller.rb
@@ -32,4 +32,12 @@ class PortfoliosController < ApplicationController
 	def show
 		@portfolio_items = Portfolio.find(params[:id])
 	end
+	def destroy
+		@portfolio_items = Portfolio.find(params[:id])
+		@portfolio_items.destroy
+		respond_to do |format|
+			format.html {redirect_to portfolios_url, notice: 'Record removed :|' }
+			format.json {head:no_content}
+		end
+	end
 end
