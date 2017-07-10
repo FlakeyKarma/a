13c9a14f3c8d9cc4920960d6985bd99cac02b6f0
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 16:33:42 +0000

    Implemented view helper for auth links

--- DevCampPortf/app/helpers/application_helper.rb
+++ DevCampPortf/app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-	def sample_help
-		"<p>My helper</p>".html_safe
+	def login_helper
+		"<br>".html_safe +
+		if current_user.is_a?(User)
+			link_to "Logout", destroy_user_session_path, method: :delete
+		else
+			(link_to "Join us!", new_user_registration_path) + "<br>".html_safe +
+			(link_to "Sign back in!", new_user_session_path)
+		end
 	end
 end
