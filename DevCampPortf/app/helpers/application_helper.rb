981d908ec7098e34b879e1be72823de315f2d2b4
Jordan Hudgens <jordan@devcamp.com>
2017-07-24 00:53:08 +0000

    a

--- DevCampPortf/app/helpers/application_helper.rb
+++ DevCampPortf/app/helpers/application_helper.rb
@@ -1,20 +1,22 @@
 module ApplicationHelper
-	def login_helper
-		"<br>".html_safe +
-		if current_user.is_a?(User)
-			link_to "Logout", destroy_user_session_path, method: :delete
-		else
-			(link_to "Join us!", new_user_registration_path) + "<br>".html_safe +
-			(link_to "Sign back in!", new_user_session_path)
-		end
-	end
-	def sourceH(layout_name)
-		    if session[:source]
-		    	a = "Thanks for visiting me on the #{layout_name} page from #{session[:source]}!!"
-				content_tag(:p, a, class: "sourceG")
-		    end
-	end
-	def copRi_g
-		FlakeykarmasViToo::Rend.copyR '♠FlakeyKarma♠', 'All rights reserved boy'
-	end
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_user_session_path, method: :delete
+    else
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
+    end
+  end
+
+  def sourceH(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+
+  def copR
+    GemV::Renderer.copyright '♠Flakey Karma♠', 'All rights reserved'
+  end
 end
