5e0ae800c48744684df3246216786e48e9373363
Jordan Hudgens <jordan@devcamp.com>
2017-07-30 18:20:44 +0000

    a

--- DevCampPortf/app/models/portfolio.rb
+++ DevCampPortf/app/models/portfolio.rb
@@ -2,10 +2,11 @@ class Portfolio < ApplicationRecord
 	
 	has_many :technologies
 	accepts_nested_attributes_for :technologies,
+		allow_destroy: true,
 		reject_if: lambda {|attrs| attrs['name'].blank?}
 	
-	include Placeholder
-	validates_presence_of :title, :body, :main_image, :thumb_image
+	#include Placeholder
+	validates_presence_of :title, :body
 	
 	def self.A
 		where(subtitle: 'Yo, I\'m Angular')
