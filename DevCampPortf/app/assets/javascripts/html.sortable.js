d17f95da542bc14490f0900ef109cb3f0e1212d2
Jordan Hudgens <jordan@devcamp.com>
2017-08-09 16:03:52 +0000

    Fixed the h\*cking problem with the comment display on blogs...and added a few other things

--- DevCampPortf/app/assets/javascripts/html.sortable.js
+++ DevCampPortf/app/assets/javascripts/html.sortable.js
@@ -1,5 +1,3 @@
-alert("Hey There Friend!")
-
 ;(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
--- DevCampPortf/app/controllers/blogs_controller.rb
+++ DevCampPortf/app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   layout "blogs"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
@@ -79,5 +80,9 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body)
+    end
+    
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
     end
 end
