e6fb19a963591ba5c0be2115942320616e5aa6ac
Jordan Hudgens <jordan@devcamp.com>
2017-08-13 06:08:04 +0000

    Implemented redirect for blog show page when non-admin tries to access draft blog

--- DevCampPortf/app/controllers/blogs_controller.rb
+++ DevCampPortf/app/controllers/blogs_controller.rb
@@ -19,11 +19,15 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
     
-    @blog_title = @blog.title
-    @seo_keywords = @blog.body
+      @blog_title = @blog.title
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "//AccessDenied"
+    end
   end
 
   # GET /blogs/new
